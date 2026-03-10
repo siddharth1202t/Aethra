87778847f945e8e8bbb9cc4429611110e67ccf5c
siddharth1202t <siddharth1202t@gmail.com>
2026-03-10 16:15:37 +0530

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,1 +1,63 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  updateProfile
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbfEQyTwry7qNOluYqlHUZuU8AF3bkpgQ",
+  authDomain: "aethra-web.firebaseapp.com",
+  projectId: "aethra-web",
+  storageBucket: "aethra-web.firebasestorage.app",
+  messagingSenderId: "280560043528",
+  appId: "1:280560043528:web:a6c2e485c8da32c9dab3bd"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const signupForm = document.getElementById("signupForm");
+const googleBtn = document.getElementById("googleSignInBtn");
+
+if (signupForm) {
+  signupForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+      await updateProfile(userCredential.user, {
+        displayName: name
+      });
+
+      alert("Signup successful!");
+      window.location.href = "home.html";
+
+    } catch (error) {
+      alert(error.message);
+    }
+  });
+}
+
+if (googleBtn) {
+  googleBtn.addEventListener("click", async () => {
+    try {
+      await signInWithPopup(auth, provider);
+
+      alert("Google login successful!");
+      window.location.href = "home.html";
+
+    } catch (error) {
+      alert(error.message);
+    }
+  });
+}
