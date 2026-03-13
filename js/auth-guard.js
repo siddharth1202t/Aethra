da167bdfbc80bc2a6eac582a2ced7e595410e18a
siddharth1202t <siddharth1202t@gmail.com>
2026-03-13 16:03:36 +0530

    Update auth-guard.js

--- js/auth-guard.js
+++ js/auth-guard.js
@@ -1,1 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { SECURITY_CONFIG } from "./security-config.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCbfEQyTwry7qNOluYqlHUZuU8AF3bkpgQ",
+  authDomain: "aethra-web.firebaseapp.com",
+  projectId: "aethra-web",
+  storageBucket: "aethra-web.firebasestorage.app",
+  messagingSenderId: "280560043528",
+  appId: "1:280560043528:web:a6c2e485c8da32c9dab3bd"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export function requireAuth(callback) {
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      window.location.href = "login.html";
+      return;
+    }
+
+    callback(user);
+  });
+}
+
+export function requireDeveloper(callback) {
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      window.location.href = "login.html";
+      return;
+    }
+
+    if (user.email !== SECURITY_CONFIG.developerEmail) {
+      window.location.href = "home.html";
+      return;
+    }
+
+    callback(user);
+  });
+}
+
+export { auth };
