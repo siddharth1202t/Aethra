fabb1c3246085c8c9b49585cd8c5d5a06f16a98e
siddharth1202t <siddharth1202t@gmail.com>
2026-03-13 16:24:34 +0530

    Create firestore-config.js

---
+++ js/firestore-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbfEQyTwry7qNOluYqlHUZuU8AF3bkpgQ",
+  authDomain: "aethra-web.firebaseapp.com",
+  projectId: "aethra-web",
+  storageBucket: "aethra-web.firebasestorage.app",
+  messagingSenderId: "280560043528",
+  appId: "1:280560043528:web:a6c2e485c8da32c9dab3bd"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
