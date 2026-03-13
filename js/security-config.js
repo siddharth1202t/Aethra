a9b32636686634ef2abd8fb59d029413604319c7
siddharth1202t <siddharth1202t@gmail.com>
2026-03-13 15:52:15 +0530

    Create security-config.js

---
+++ js/security-config.js
@@ -0,0 +1,7 @@
+export const SECURITY_CONFIG = {
+  developerEmail: "siddharthkumar9127@gmail.com",
+  allowedOrigins: [
+    "https://aethra-gules.vercel.app",
+    "https://aethra-hb2h.vercel.app"
+  ]
+};
