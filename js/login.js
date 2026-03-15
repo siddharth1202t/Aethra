71049abb653c769d66bf75d411bd69ce9a61daf8
siddharth1202t <siddharth1202t@gmail.com>
2026-03-15 12:04:31 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import {
   getAuth,
   signInWithEmailAndPassword,
@@ -9,18 +8,9 @@ import {
   sendPasswordResetEmail,
   reload
 } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { app } from "./firestore-config.js";
 import { ensureUserProfile } from "./user-profile.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCbfEQyTwry7qNOluYqlHUZuU8AF3bkpgQ",
-  authDomain: "aethra-web.firebaseapp.com",
-  projectId: "aethra-web",
-  storageBucket: "aethra-web.firebasestorage.app",
-  messagingSenderId: "280560043528",
-  appId: "1:280560043528:web:a6c2e485c8da32c9dab3bd"
-};
-
-const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
