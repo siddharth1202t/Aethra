76831f135e20796ea9f1487e8b8f34681f064c54
siddharth1202t <siddharth1202t@gmail.com>
2026-03-17 20:10:03 +0530

    Update firestore-config.js

--- js/firestore-config.js
+++ js/firestore-config.js
@@ -8,11 +8,6 @@ import {
   getFirestore
 } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
-/*
-  Firebase initialization for Aethra.
-  Ensures the app is created only once even if multiple modules import this file.
-*/
-
 const firebaseConfig = Object.freeze({
   apiKey: "AIzaSyCbfEQyTwry7qNOluYqlHUZuU8AF3bkpgQ",
   authDomain: "aethra-web.firebaseapp.com",
@@ -22,24 +17,7 @@ const firebaseConfig = Object.freeze({
   appId: "1:280560043528:web:a6c2e485c8da32c9dab3bd"
 });
 
-let app;
-let db;
-
-function initializeFirebase() {
-  try {
-    if (getApps().length > 0) {
-      app = getApp();
-    } else {
-      app = initializeApp(firebaseConfig);
-    }
-
-    db = getFirestore(app);
-  } catch (error) {
-    console.error("Firebase initialization failed:", error);
-    throw new Error("Firebase initialization error");
-  }
-}
-
-initializeFirebase();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export { app, db };
