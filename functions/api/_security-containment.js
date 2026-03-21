5b1382514e929fc514bb715c7e2d83e2fe2ed728
siddharth1202t <siddharth1202t@gmail.com>
2026-03-21 19:23:30 +0530

    Update _security-containment.js

--- functions/api/_security-containment.js
+++ functions/api/_security-containment.js
@@ -1,5 +1,5 @@
 import { redis } from "./_redis.js";
-import { appendSecurityEvent } from "./_security-events-store.js";
+import { appendSecurityEvent } from "./_security-event-store.js";
 
 const CONTAINMENT_KEY = "security:containment:global";
 const CONTAINMENT_TTL_MS = 24 * 60 * 60 * 1000;
